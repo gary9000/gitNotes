619a872396c5de2d2224268006aecddb86c5cb97
gary9 <gary9000unix@yahoo.com>
2022-05-23 12:14:36 -0500

    Update and rename README.md to gitCommands.cs

--- README.md
+++
@@ -1,2 +0,0 @@
-# gitNotes
-gitCommands
---
+++ gitCommands.cs
@@ -0,0 +1,34 @@
+# gitNotes
+gitCommands
+
+using System;
+
+public class Class1
+{
+	public Class1()
+	{
+	}
+}
+
+/*
+ * git video refresher for work
+ * 
+ * 
+ * 01. git add                                           for staging
+ * 02. git commit                                        for 
+ * 03. git push                                          to github
+ * 04. git pull                                          to retrieve from github or aws or server
+ * 05. git config user.email "gary9000unix@yahoo.com"    initial
+ * 06. git rm -r --cached .                              rm git additions, from staging
+ * 07. git commit -m "message"                           can commit as many times as needed
+ * 08. git log                                           all data and hash
+ * 09. git show b896d01651efcaac930b78a66ced6ce7bf79a5de with hash will show diff
+ * 10. git cat file                                      to show file not git though
+ * 11. git diff                                          to show
+ * 12. git restore                                       a previous restored
+ * 13. git commit --amend -m "amend the message"         to amend
+ * 14. git remote add origin github https                to prep for push
+ * 15. git push origin main                              to push
+ * 16. git clone                                         brign a repos that is hosted somewhere like github into a local
+ 
+ */
